8e3ab5d5861eca2989eabe45afe8c7859397915d
shannonyuna <32002312+shannonyuna@users.noreply.github.com>
2017-10-07 05:04:32 -0400

    hw

--- work/shannon/homework/sketch.js
+++ work/shannon/homework/sketch.js
@@ -1,9 +1,7 @@
+var hueRand1;
+var hueRand2;
+var hueRand3;
 
-var myHue = 0;
-var speedX = 1;
-var speedY = 10;
-var circleX = 0;
-var circleY = 100;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -11,92 +9,76 @@ function setup() {
 };
 
 function draw(){
-  var hueRandomnumber= random(255);
-  myHue +=4;
-
-  fill(myHue, 100, 100);
   noStroke();
 
   if (mouseIsPressed){
     createBalloon();
+    createConfetti();
+    createTri();
   };
 
-  if (myHue > 360) {
-    myHue=0;
+  if (hueRand1 > 360){
+    hueRand1 = 0;
+  };
+  if (hueRand2 > 360){
+    hueRand2 = 0;
+  };
+  if (hueRand3 > 360){
+    hueRand3 = 0;
   };
 
   if (keyIsPressed){
     background(255);
   };
-}
+};
 
 function createBalloon(){
+  var hueRand1= random(255);
+
+  var xPos = random(-200,200);
+  var yPos = random(-200,200);
   var numOfLoop = 100;
-  var circleSize = 5 + random(100);
+  var circleSize = random(100);
+  var randomB = random(50);
 
   for (var i = 0; i < numOfLoop; i++){
-    var theta = i * ((Math.PI*2)/ numOfLoop) * (mouseY*0.1);
-    var radius = 0.01 * i * mouseX;
-    var xPos = Math.cos(theta) * radius
-    var yPos = Math.sin(theta) * radius
-
-    noStroke();
-    fill (myHue, 100, 100);
-    ellipse(mouseX, mouseY, circleSize, circleSize);
+    fill(hueRand1, randomB, 100);
+    ellipse(mouseX+xPos, mouseY+yPos, circleSize, circleSize);
   };
+};
 
-  if (circleX > width || circleX < 0 ){
-  speedX = speedX * -1;
-  }
-  if (circleY > height || circleY < 0){
-    speedY = speedY * -1;
-  }
+function createConfetti(){
+  var hueRand2= random(255);
 
-  circleX = circleX + speedX;
-  circleY = circleY + speedY;
-}
-//
-// var myHue = 0;
-// var circleX = 0;
-// var circleY = 100;
-// var speedX = 1;
-// var speedY = 0.5;
-//
-// function draw() {
-//
-//     colorMode(HSB);
-//     var xPos = random(100)-50;
-//     var yPos = random(100)-50;
-//
-//     var circleSize = 5 + random(100);
-//     var hueRandomnumber= random(255);
-//     myHue +=4;
-//
-//     fill(myHue, 100, 100);
-//     stroke(0);
-//     strokeWeight(1);
-//     noStroke();
-//     ellipse(circleX, circleY, 30, 30);
-//
-//     if (circleX > width || circleX < 0 ){
-//     speedX = speedX * -1;
-//     }
-//     if (circleY > height || circleY < 0){
-//       speedY = speedY * -1;
-//     }
-//
-//   if (myHue > 360) {
-//     myHue=0;
-//   }
-//
-//   if (mouseIsPressed){
-//     noStroke();
-//     ellipse(mouseX+xPos, mouseY+yPos, circleSize, circleSize);
-//     }
-//
-//   if (keyIsPressed){
-//     background(255);
-//   }
-//   circleX = circleX + speedX;
-//   circleY = circleY - speedY;
-// }
+  var xRectPos = random(-200,200);
+  var yRectPos = random(-200,200);
+  var numOfLoop = 100;
+  var rectSizeX = random(80);
+  var rectSizeY = random(80);
+  var randomB = random(50);
+
+  for (var i = 0; i < numOfLoop; i++){
+    fill(hueRand2, randomB, 100);
+    rect(mouseX+xRectPos, mouseY+yRectPos, rectSizeX, rectSizeY);;
+  };
+};
+
+function createTri(){
+var hueRand3= random(255);
+
+  var triRandom = random(100)
+  var xTriPos1 = random(triRandom *-1, triRandom *+1)+ triRandom
+  var yTriPos1 = random(triRandom *-1, triRandom *+1)+ triRandom
+  var xTriPos2 = random(triRandom *-1, triRandom *+1)+ triRandom
+  var yTriPos2 = random(triRandom *-1, triRandom *+1)+ triRandom
+  var xTriPos3 = random(triRandom *-1, triRandom *+1)+ triRandom
+  var yTriPos3 = random(triRandom *-1, triRandom *+1)+ triRandom
+
+  var numOfLoop = 100;
+  var randomB = random(50);
+
+  for (var i = 0; i < numOfLoop; i++){
+    fill(hueRand3, randomB, 100);
+    triangle(mouseX+xTriPos1, mouseY+yTriPos1, mouseX+xTriPos2, mouseY+yTriPos2, mouseX+xTriPos3, mouseY+yTriPos3 );
+  };
+};
