6519f68f5f860b27fb4e7cedb24e72f16c9b8802
woojin00 <31783607+woojin00@users.noreply.github.com>
2017-11-10 16:28:19 -0500

    2

--- work/woojin/homework2/sketch.js
+++ work/woojin/homework2/sketch.js
@@ -1,31 +1,27 @@
-var cat;
-var catImg;
-
-function preload() {
-  catImg = loadImage("01.png");
-}
 
 function setup() {
   createCanvas(500, 500);
 }
 
 function draw() {
-  background(0);
+  background(0, 30, 50);
   for (var i = 0; i < allSprites.length; i++) {
     allSprites[i].addSpeed(0.1, 90);
     if (allSprites[i].position.y > height) {
       allSprites[i].velocity.y *= -1;
     }
     if (allSprites[i].position.x > width ||
-        allSprites[i].position.x < 0) {
-      allSprites[i].remove();
+      allSprites[i].position.x < 0) {
+      allSprites[i].velocity.x *= -1;
     }
   }
+
   fill(255);
   textSize(10);
-  textAlign(LEFT, TOP);
+  textAlign(RIGHT, TOP);
   text("count: " + allSprites.length,
-    450, height-20);
+    width-10, height-20);
+  textAlign(LEFT, TOP);
   text("Woojin Chung", 100, height-20);
   text("hw02", 10, height-20);
   drawSprites();
