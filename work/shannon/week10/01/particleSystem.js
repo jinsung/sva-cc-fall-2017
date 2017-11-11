964abeafd6d655c8d8240324b771bb9a2c2fac5a
mpark31 <32002309+mpark31@users.noreply.github.com>
2017-11-10 21:16:51 -0500

    wk10

---
+++ work/shannon/week10/01/particleSystem.js
@@ -0,0 +1,46 @@
+function ParticleSystem (){
+  this.particles = [];
+  //array where to store the ParticleSystem
+
+  //need to specify where the particles are and the number
+  this.addParticles = function(pos, numOfParticles){
+    for (var i = 0; i < numOfParticles; i++){
+      var p = new Particle(); //class starts with Uppercase
+      var pos = createVector(pos.x + random(-5,5),pos.y + random(-5,5));
+      p.setup(pos);
+      var force = createVector(random(-0.5,0.5),random(-0.5, 0.5));
+      p.applyForce(force);
+      this.particles.push(p);
+      //object is assigned to array
+      //this.particles[i] = p : loop once
+    }
+  }
+
+  this.dontTouch = function(){
+    for(var i = 0; i < this.particles.length; i++){
+      var ip = this.particles[i];
+      for (var j = this.particles.length -1; j > i; j --){
+        var jp = this.particles[j];
+        var distance = ip.pos.dist(jp.pos); //distance between ip.pos and jp.pos
+        if (distance < (ip.size + jp.size)){
+          var f1 = ip.pos.copy().sub(jp.pos).mult(0.001);//push forward
+          var f2 = ip.pos.copy().sub(jp.pos).mult(-0.001);//push backward
+          ip.applyForce(f1);
+          jp.applyForce(f2);
+        }
+      }
+    }
+  }
+
+  this.draw = function(){
+    for(var i = 0; i < this.particles.length; i++){
+      if(this.particles[i].isDead){
+        this.particles.slice(i,1);
+      } else{
+        this.particles[i].update();
+        this.particles[i].draw();
+      }
+    }
+    this.dontTouch();
+  }
+}
