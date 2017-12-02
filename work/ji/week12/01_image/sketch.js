775ffa8b4e01099c260c1c5d0f93d108e036f28c
jisoo966 <32002313+jisoo966@users.noreply.github.com>
2017-12-01 21:40:15 -0500

    Ji 12/01/17

---
+++ work/ji/week12/01_image/sketch.js
@@ -0,0 +1,32 @@
+var myImage;
+
+function preload() {
+  myImage = loadImage("assets/image.jpg");
+}
+
+function setup() {
+  createCanvas(366, 542);
+}
+
+function draw() {
+  background(200);
+  image(myImage, 0, 0, width, height);
+
+  // noStroke();
+  var pd = pixelDensity();
+  loadPixels();
+
+  fill(0);
+  rect(0, 0, width, height);
+  // console.log("how many numbers you have? " + pixels.length + " " + pd);
+  var gap = 5;
+  var size = 5;
+  for (var y = 0; y < height; y += gap) {
+    for (var x = 0; x < width; x += gap) {
+      var pixelIndex = ((x + (y * width) * pd) * pd) * 4
+      fill(pixels[pixelIndex], pixels[pixelIndex + 1], pixels[pixelIndex + 2], pixels[pixelIndex + 3]);
+      // fill(pixels[pixelIndex]);
+      ellipse(x, y, size, size);
+    }
+  }
+}
