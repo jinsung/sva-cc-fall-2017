1d8d6767a773c2e5ab0577347898286444ad5057
jinsungsva <31589495+jinsungsva@users.noreply.github.com>
2017-12-01 21:42:57 -0500

    week12 work

---
+++ work/jinsung/week12/02_particleSystem_with_image/sketch.js
@@ -0,0 +1,27 @@
+var myImage;
+var particleSystem;
+
+function preload() {
+  myImage = loadImage("assets/rose.png");
+}
+
+function setup() {
+  createCanvas(500, 333);
+  particleSystem = new ParticleSystem();
+  particleSystem.setup();
+  noStroke();
+}
+
+function draw() {
+  background(200);
+  image(myImage, 0, 0, width, height);
+  loadPixels();
+  var myPixels = pixels;
+
+  if (mouseIsPressed) {
+    particleSystem.addParticle(mouseX, mouseY, 1);
+  }
+  background(100);
+  particleSystem.draw( myPixels );
+
+}
