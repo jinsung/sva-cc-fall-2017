91fa965971ae6fc0a047a264ee660619c29f4c53
woojin00 <31783607+woojin00@users.noreply.github.com>
2017-12-08 21:11:42 -0500

    week 12 week 13

---
+++ work/Minyoung/week 13/01_arrow/target.js
@@ -0,0 +1,17 @@
+function Target() {
+  this.setup = function(pos, numOfCircles, size) {
+    this.pos = pos;
+    this.numOfCircles = numOfCircles;
+    this.size = size;
+  }
+
+  this.draw = function() {
+    stroke (0);
+    for (var i=this.numOfCircles; i>0; i--) {
+      var step = i/this.numOfCircles; // range from 1 to 0.
+      fill(step*255);
+      var segSize = this.size * step;
+      ellipse(this.pos.x, this.pos.y, segSize, segSize);
+    }
+  }
+}
