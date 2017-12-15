b47d8c9fd58e72dfca2f45765f60f7e3553ad948
anuka8 <ayavuukhulan@sva.edu>
2017-12-15 18:33:02 -0500

    1

---
+++ work/woojin/final/sketch.js
@@ -0,0 +1,277 @@
+var key = 'AIzaSyCn_28z64SaQdTPxRBNEn15FXudNr5YoH0'
+
+var style = [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#212121"
+      }
+    ]
+  },
+  {
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#212121"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#757575"
+      },
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.country",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.locality",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.neighborhood",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#181818"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1b1b1b"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#2c2c2c"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#8a8a8a"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#373737"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#3c3c3c"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#4e4e4e"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#000000"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#3d3d3d"
+      }
+    ]
+  }
+]
+
+var options = {
+  lat: 37.566490,
+  lng: 126.977813,
+  zoom: 0,
+  styles: style
+}
+
+var mappa = new Mappa('Google', key);
+var myMap;
+var t;
+var canvas;
+
+function setup(){
+  canvas = createCanvas(800, 700);
+  myMap = mappa.tileMap(options);
+  myMap.overlay(canvas);
+  noFill();
+  noStroke();
+  t = 0;
+}
+
+function draw(){
+  clear();
+  colorMode (HSB);
+
+  var x = width * noise(t);
+  var y = height * noise(t+5);
+  var r = 255 * noise(t+10);
+  var g = 255 * noise(t+15);
+  var b = 255 * noise(t+20);
+
+   noStroke();
+   fill(r, g, b);
+   ellipse(x, y, 10, 10);
+   t = t + 0.01;
+}
---
+++ work/woojin/week13/01_arrow/arrow.js
@@ -0,0 +1,69 @@
+function Arrow () {
+  this.setup = function (pos, size) {
+    this.pos = pos.copy();
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.mass = 1;
+    this.damping = 0.98;
+    this.size = size;
+    this.isActive = false;
+    this.angle = 0;
+  }
+
+  this.active = function () {
+    this.isActive = true;
+  }
+
+  this.applyForce = function (force) {
+    if(this.isActive) {
+      var f = force.copy().div(this.mass);
+      this.acc.add(f);
+    }
+  }
+
+  this.update = function () {
+    this.calcAngle();
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+    this.vel.mult(this.damping);
+  }
+
+  this.stop = function () {
+    this.vel.mult(0);
+  }
+
+  this.draw = function () {
+    push();
+    fill(0);
+    translate(this.pos.x, this.pos.y);
+    rotate(this.angle);
+    triangle(0, 0, 10, 3, 10, -3);
+    line(0, 0, this.size, 0 );
+
+    // draw feathers
+    var featherPos = 0.7;
+    var featherSize = 5;
+    var numOfFeathers = 4;
+    for (var i = 0; i < numOfFeathers; i++) {
+      this.drawFeather(this.size * featherPos, featherSize);
+      featherPos += 0.1;
+    }
+    pop();
+  }
+
+  this.drawFeather = function (pos, size) {
+    line (pos, 0, pos + size, size);
+    line (pos, 0, pos + size, -size);
+  }
+
+  this.calcAngle = function () {
+    if (this.isActive) {
+      this.angle = Math.atan2(this.vel.y, this.vel.x) + Math.PI;
+    } else {
+      var mousePos = createVector (mouseX, mouseY);
+      var diffVector = p5.Vector.sub (mousePos, myArrow.pos);
+      this.angle = Math.atan2 (diffVector.y, diffVector.x) + Math.PI;
+    }
+  }
+}
